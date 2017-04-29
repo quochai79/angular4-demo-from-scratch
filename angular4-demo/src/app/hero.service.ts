6a9dcbe22ea47094219318a53b0bf64cc27ee43b
Hai Doan <quochai07@gmail.com>
2017-04-29 17:20:10 +0700

    Routing

---
+++ angular4-demo/src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+  
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+}
